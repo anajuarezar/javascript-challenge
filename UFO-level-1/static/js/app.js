a4a1d3e8fe9dcb7907895c5af61db5883f6f99e3
anajuarezar <ana.juarezar@gmail.com>
2021-06-25 17:20:51 -0500

    Table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+data.forEach(function(UFOReport) {
+  console.log(UFOReport);
+  var row = tbody.append("tr");
+  Object.entries(UFOReport).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
